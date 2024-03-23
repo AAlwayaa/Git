adf7bae28484ba0f287acef86b813a122bd520a7
AAlwayaa <164499150+AAlwayaa@users.noreply.github.com>
2024-03-23 22:08:21 +0800

    第三次提交

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    private string str = "Third";
+    private string str = "Eight";
     // Start is called before the first frame update
     void Start()
     {
